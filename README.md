ac6851d92299971a0331e27e57ae1875cb6e5a95
yliu0418 <yliu0418@student.monash.edu>
2023-01-02 15:33:04 +1100

    modified:   README.md modified:   frontend/src/App.js new file:   frontend/src/screens/ProductScreen.js modified:   frontend/package-lock.json modified:   frontend/package.json modified:   frontend/src/index.css frontend/public/images/ frontend/src/data.js frontend/src/screens/HomeScreen.js

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 2. Install Tools
 3. Create React App
 4. Create Git Repository
+5. List Products
+   1. create products array
+   2. add product images
+   3. render products
+   4. style products
+6. Add routing
+   1. npm i react-router-dom
+   2. create route for home screen
+   3. create router for product screen
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
